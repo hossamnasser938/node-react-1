66f0ff0240abf102c19737cd8c52c55640d2e6e3
hossamnasser938 <hossamnasser938@gmail.com>
2023-09-10 00:55:34 +0300

    serve react app within node app

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from node server" });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`node server is listening on port ${PORT}`);
